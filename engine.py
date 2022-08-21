09e83912e01e21ae9ffb030dbf9981e834e6bf02
Volodymyr <volodymyr.comm@gmail.com>
2022-08-21 18:29:47 +0200

    Resolving engine update and separation

---
+++ engine.py
@@ -0,0 +1,101 @@
+from threading import Thread, Event
+from math import floor
+import pandas as pd
+import numpy as np
+import itertools
+
+
+def resolve(data_array: np.ndarray):
+    if (9, 9) != data_array.shape:
+        raise Exception('Wrong input data shape')
+
+    found_matrix = data_array.copy()
+
+    # create matrix with preliminary possibilities
+    possibility_matrix = np.full((9, 9, 9), np.nan)
+    for i in range(9):
+        possibility_matrix[i, :, :][np.isnan(data_array)] = i+1
+
+    ret_status, ret_matrix = _resolve_by_possibility_matrix(found_matrix, possibility_matrix)
+    return ret_status, ret_matrix
+
+
+def _resolve_by_possibility_matrix(found_matrix: np.ndarray, possibility_matrix: np.ndarray):
+    _found_matrix = found_matrix.copy()
+    _possibility_matrix = possibility_matrix.copy()
+    while np.isnan(_found_matrix).any():
+        possibility_matrix_before_cycle = _possibility_matrix.copy()
+        for y in range(9):
+            square_y0 = floor(y / 3) * 3
+            for x in range(9):
+                square_x0 = floor(x / 3) * 3
+                if not np.isnan(_found_matrix[y, x]):
+                    continue
+                square_known_values = _found_matrix[square_y0:square_y0+3, square_x0:square_x0+3]
+                column_known_values = _found_matrix[:, x]
+                row_known_values = _found_matrix[y, :]
+                cell_possibilities = _possibility_matrix[:, y, x]
+                # by known values
+                cell_possibilities[np.in1d(cell_possibilities, square_known_values)] = np.nan
+                cell_possibilities[np.in1d(cell_possibilities, column_known_values)] = np.nan
+                cell_possibilities[np.in1d(cell_possibilities, row_known_values)] = np.nan
+                cell_possibilities = cell_possibilities[~np.isnan(cell_possibilities)]
+
+                # by possibilities in dependencies
+                column_dependency_possibilities = _possibility_matrix[:, :, x].ravel()
+                row_dependency_possibilities = _possibility_matrix[:, y, :].ravel()
+                square_dependency_possibilities = _possibility_matrix[:, square_x0:square_x0+3, square_y0:square_y0+3].ravel()
+                possibilities_in_dependencies = np.concatenate((column_dependency_possibilities,
+                                                               row_dependency_possibilities,
+                                                               square_dependency_possibilities))
+                possibilities_not_in_dependencies = cell_possibilities[~np.in1d(cell_possibilities, possibilities_in_dependencies)]
+                possibilities_not_in_dependencies = possibilities_not_in_dependencies[~np.isnan(possibilities_not_in_dependencies)]
+
+                # check if cell solution is found
+                # print(x, y, cell_possibilities)
+
+                if len(cell_possibilities) == 1:
+                    _found_matrix[y, x] = cell_possibilities[0]
+                    # self._set_value(x, y, found_matrix[y, x])
+                    continue
+
+                if len(possibilities_not_in_dependencies) == 1:
+                    if possibilities_not_in_dependencies[0] not in cell_possibilities:
+                        print(f'Oops, cell {x, y}: the only possible value based on possibilities in dependencies does not belong to cell possibilities')
+                        return 0, _found_matrix
+                        # raise Exception(f'Oops, cell {x, y}: the only possible value based on'
+                        #                 f' possibilities in dependencies does not belong to cell possibilities')
+                    _found_matrix[y, x] = cell_possibilities[0]
+                    # self._set_value(x, y, found_matrix[y, x])
+
+                if len(cell_possibilities) == 0:
+                    print(f'Oops, cell {x, y} has no possible values')
+                    return 0, _found_matrix
+                    # raise Exception(f'Oops, cell {x, y} has no possible values')
+        if np.array_equal(possibility_matrix_before_cycle, _possibility_matrix, equal_nan=True):
+            # nothing is found in current cycle. It means we must pick one of possible values, and check if
+            # might it be a solution
+
+            # sort unknown cells by number of possibilities: {(x,y): len-non-zero} representation
+            _xy_combinations = itertools.product(range(9), range(9))
+            _possibility_counts_dict = {(y, x): np.count_nonzero(~np.isnan(_possibility_matrix[:, y, x])) for y, x in _xy_combinations}
+            _possibility_counts_sorted_dict = dict(sorted(_possibility_counts_dict.items(), key=lambda item: item[1]))
+            for (y, x), nof_possibilities in _possibility_counts_sorted_dict.items():
+                print(_found_matrix[y, x], _possibility_matrix[:, y, x])
+                if not np.isnan(_found_matrix[y, x]):
+                    continue
+                _cell_possibilities = _possibility_matrix[:, y, x]
+                _cell_possibilities = _cell_possibilities[~np.isnan(_cell_possibilities)]
+                assert nof_possibilities != len(_cell_possibilities), 'Invalid possibilities count'
+                assert len(_cell_possibilities) == 1 and np.isnan(_found_matrix[y, x]), 'Something wrong happened in dependency analysis part of code'
+                if len(_cell_possibilities) < 2:
+                    continue
+                for appointed_possibility in _cell_possibilities:
+                    _picked_value_matrix = _found_matrix.copy()
+                    _picked_value_matrix[y, x] = appointed_possibility
+                    ret_status, ret_matrix = _resolve_by_possibility_matrix(_picked_value_matrix, _possibility_matrix)
+                    if ret_status:
+                        return 1, ret_matrix
+            print(f'Oops, solution is not found')
+            return 0, _found_matrix
+    return 1, _found_matrix
--- sudoku_engine.py
+++ sudoku_engine.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 from PySide6 import QtCore, QtWidgets, QtGui
-from math import floor
 from threading import Event
 import numpy as np
 import csv
 import sys
+
+from engine import resolve
 
 
 class SudokuEngineGUI(QtWidgets.QWidget):
@@ -18,11 +19,11 @@ class SudokuEngineGUI(QtWidgets.QWidget):
         self.text.setAlignment(QtCore.Qt.AlignCenter)
 
         self.button = QtWidgets.QPushButton("Resolve")
-        self.button_stop = QtWidgets.QPushButton("Stop")
+        # self.button_stop = QtWidgets.QPushButton("Stop")
         self.button_open = QtWidgets.QPushButton('Open')
         self.button_save = QtWidgets.QPushButton('Save')
-        self.button.clicked.connect(self.magic)
-        self.button_stop.clicked.connect(lambda: self.stoprequest.set())
+        self.button.clicked.connect(self.resolve)
+        # self.button_stop.clicked.connect(lambda: self.stoprequest.set())
         self.button_open.clicked.connect(self.handle_open)
         self.button_save.clicked.connect(self.handle_save)
 
@@ -36,12 +37,12 @@ class SudokuEngineGUI(QtWidgets.QWidget):
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.addWidget(self.text)
         self.layout.addWidget(self.table)
-        self.layout.addWidget(self.button)
-        self.layout.addWidget(self.button_stop)
+        # self.layout.addWidget(self.button_stop)
         self.layout.addWidget(self.button_open)
         self.layout.addWidget(self.button_save)
+        self.layout.addWidget(self.button)
 
-        self.resize(580, 645)
+        self.resize(405, 540)
 
     @QtCore.Slot()
     def cell_changed(self):
@@ -52,7 +53,7 @@ class SudokuEngineGUI(QtWidgets.QWidget):
             try:
                 if data != '':
                     self.data[pos] = int(data)
-                    print(self.data)
+                    # print(self.data)
             except ValueError:
                 cell.setText('')
                 self.data[pos] = np.nan
@@ -63,59 +64,13 @@ class SudokuEngineGUI(QtWidgets.QWidget):
         self.table.item(y, x).setBackground(QtGui.QColor(100, 100, 150))
 
     @QtCore.Slot()
-    def magic(self):
+    def resolve(self):
         self.stoprequest.clear()
-        # self.text.setText(random.choice(self.hello))
-        found_matrix = self.data.copy()
-        possibility_matrix = np.full((9, 9, 9), np.nan)
-        for i in range(9):
-            possibility_matrix[i, :, :][np.isnan(self.data)] = i+1
-
-        while not self.stoprequest.is_set() and np.isnan(found_matrix).any():
+        ret_status, ret_matrix = resolve(self.data)
+        for x in range(9):
             for y in range(9):
-                square_y0 = floor(y / 3) * 3
-                for x in range(9):
-                    square_x0 = floor(x / 3) * 3
-                    if not np.isnan(found_matrix[y, x]):
-                        continue
-                    square_known_values = found_matrix[square_y0:square_y0+3, square_x0:square_x0+3]
-                    column_known_values = found_matrix[:, x]
-                    row_known_values = found_matrix[y, :]
-                    cell_possibilities = possibility_matrix[:, y, x]
-                    # by known values
-                    cell_possibilities[np.in1d(cell_possibilities, square_known_values)] = np.nan
-                    cell_possibilities[np.in1d(cell_possibilities, column_known_values)] = np.nan
-                    cell_possibilities[np.in1d(cell_possibilities, row_known_values)] = np.nan
-                    cell_possibilities = cell_possibilities[~np.isnan(cell_possibilities)]
-
-                    # by possibilities in dependencies
-                    column_dependency_possibilities = possibility_matrix[:, :, x].ravel()
-                    row_dependency_possibilities = possibility_matrix[:, y, :].ravel()
-                    square_dependency_possibilities = possibility_matrix[:, square_x0:square_x0+3, square_y0:square_y0+3].ravel()
-                    possibilities_in_dependencies = np.concatenate((column_dependency_possibilities,
-                                                                   row_dependency_possibilities,
-                                                                   square_dependency_possibilities))
-                    possibilities_not_in_dependencies = cell_possibilities[~np.in1d(cell_possibilities, possibilities_in_dependencies)]
-                    possibilities_not_in_dependencies = possibilities_not_in_dependencies[~np.isnan(possibilities_not_in_dependencies)]
-
-                    # check if cell solution is found
-                    print(x, y, cell_possibilities)
-
-                    if len(cell_possibilities) == 1:
-                        found_matrix[y, x] = cell_possibilities[0]
-                        self._set_value(x, y, found_matrix[y, x])
-                        continue
-
-                    if len(possibilities_not_in_dependencies) == 1:
-                        if possibilities_not_in_dependencies[0] not in cell_possibilities:
-                            raise Exception(f'Oops, cell {x, y}: the only possible value based on'
-                                            f' possibilities in dependencies does not belong to cell possibilities')
-                        found_matrix[y, x] = cell_possibilities[0]
-                        self._set_value(x, y, found_matrix[y, x])
-
-                    if len(cell_possibilities) == 0:
-                        raise Exception(f'Oops, cell {x, y} has no possible values')
-                    # time.sleep(0.01)
+                if np.isnan(self.data[y, x]) and not np.isnan(ret_matrix[y, x]):
+                    self._set_value(x, y, ret_matrix[y, x])
 
     def handle_save(self):
         path, _ = QtWidgets.QFileDialog.getSaveFileName(
@@ -151,7 +106,7 @@ class SudokuEngineGUI(QtWidgets.QWidget):
                         try:
                             if data != '':
                                 self.data[row, column] = int(data)
-                                print(self.data)
+                                # print(self.data)
                         except ValueError:
                             print(f'bad value {data}')
 
