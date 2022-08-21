3bac0b619715e1be9751093bdd6df268186fcb42
Volodymyr <volodymyr.comm@gmail.com>
2022-08-21 19:20:06 +0200

    Basic setup.py intro

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='sudoku-resolver',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='volodymyr-comm',
+    author_email='',
+    description='',
+    install_requires=[
+        'setuptools~=60.2.0',
+        'pandas~=1.4.3',
+        'numpy~=1.23.0',
+        'PySide6~=6.3.1',
+    ]
+)
